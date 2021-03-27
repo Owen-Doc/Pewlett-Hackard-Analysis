7e9dd18a11acd9dcb4173b494835aeadb01ffcc2
Owen Dougherty <76958825+Owen-Doc@users.noreply.github.com>
2021-03-27 15:06:12 -0400

    Finished module schema

--- schema.sql
+++ schema.sql
@@ -40,8 +40,7 @@ create table titles(
 	title varchar(40) not null,
 	from_date date not null,
 	to_date date not null,
-	foreign key (emp_no) references employees (emp_no),
-	primary key (emp_no)
+	foreign key (emp_no) references employees (emp_no)
 );
 
 create table dept_emp (
@@ -54,4 +53,117 @@ foreign key (dept_no) references departments(dept_no),
 	primary key (emp_no, dept_no)
 );
 
+select first_name, last_name
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31')
+;
 
+-- Number of employees retiring, using count function
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creates retirement_info table to be exported as csv
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31')
+;
+
+-- Create new table for retiring employees
+select emp_no, first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31')
+and (hire_date between '1985-01-01' and '1988-12-31');
+-- Check the table
+select * from retirement_info;
+
+-- joining departments and dept_manager tables
+select departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+from departments
+inner join dept_manager
+on departments.dept_no = dept_manager.dept_no
+;
+
+-- joining retirement_info and dept_emp tables
+select retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+from retirement_info
+left join dept_emp
+on retirement_info.emp_no = dept_emp.emp_no;
+
+-- joining retirement_info and dept_emp tables
+select ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+from retirement_info as ri
+left join dept_emp as de
+on ri.emp_no = de.emp_no;
+
+-- joining departments and dept_manager tables
+select d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+from departments as d
+inner join dept_manager as dm
+on d.dept_no = dm.dept_no;
+
+-- Employee count by department numner
+select count(ce.emp_no), de.dept_no
+from current_emp as ce
+left join dept_emp as de
+on ce.emp_no = de.emp_no
+group by de.dept_no
+order by de.dept_no;
+
+select e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+-- into emp_info
+from employees as e
+inner join salaries as s
+on (e.emp_no = s.emp_no)
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+where (e.birth_date between '1952-01-01' and '1955-12-31')
+and (e.hire_date between '1985-01-01' and '1988-12-31')
+and (de.to_date = '9999-01-01')
+;
+
+-- List of managers per department
+select dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+-- into manager_info
+from dept_manager as dm
+	inner join departments as d
+		on (dm.dept_no = d.dept_no)
+	inner join current_emp as ce
+		on (dm.emp_no = ce.emp_no)
+;
+
+-- Add departments to current_emp table
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- into dept_info
+from current_emp as ce
+	inner join dept_emp as de
+		on (ce.emp_no = de.emp_no)
+	inner join departments as d
+		on (de.dept_no = d.dept_no)
+;
